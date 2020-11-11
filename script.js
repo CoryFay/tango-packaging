c573c4c0a6171df67a6e5e970897f4234ddf3d1d
Cory <coryfay97@gmail.com>
2020-11-10 22:09:12 -0800

    create an html button and empty div. create logic to append text to empty div. disregard this, but can use as examply for appending text to desired location.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+//target the 'empty-div' div
+var emptyDiv = $('#empty-div');
+//on-click event.. append text to the empty-div 
+$('#testBtn').on('click', function(){
+    for (var i = 0; i < 100 ; i++){
+    var text = 'lorem ';
+    emptyDiv.append(text);
+    }
+    
+})
+
 
 });
