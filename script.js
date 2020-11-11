217a42e0c8122da7304e351a48734eb4eac71ea7
Cory <coryfay97@gmail.com>
2020-11-11 14:19:37 -0800

    add water.css and materialize.css (water is subject to change). add materialize.js cdn as well. place a few example input boxes in html.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 var emptyDiv = $('#empty-div');
 //on-click event.. append text to the empty-div 
 $('#testBtn').on('click', function(){
+    event.preventDefault();
     for (var i = 0; i < 100 ; i++){
     var text = 'lorem ';
     emptyDiv.append(text);
