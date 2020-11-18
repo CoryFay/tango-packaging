cc3d7be02ba3cf7c67e2830fcacdbbb40eef16d6
Joe Giampaoli <giampaoli.joe@gmail.com>
2020-11-18 10:32:49 -0800

    submit button to open preview.html, new preview page for pdfs

---
+++ pdfscript.js
@@ -0,0 +1,21 @@
+var input = window.localStorage.getItem('companyList');
+console.log(input)
+
+
+
+
+
+
+
+
+var palletCustomerAddressName = $('#pallet-customer-address').children('li')[0];
+var palletCustomerAddressStreet = $('#pallet-customer-address').children('li')[1];
+var palletCustomerAddressCity = $('#pallet-customer-address').children('li')[2];
+var palletCustomerAddressPhone = $('#pallet-customer-address').children('li')[3];
+var palletCustomerAddressPO = $('#pallet-customer-address').children('li')[4];
+
+palletCustomerAddressName.innerText = 'hello';
+palletCustomerAddressStreet.innerText = 'hello';
+palletCustomerAddressCity.innerText = 'hello';
+palletCustomerAddressPhone.innerText = 'hello';
+palletCustomerAddressPO.innerText = 'hello';
