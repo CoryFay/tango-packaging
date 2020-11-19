8348514e1d8f502807752537b1a57f735eff3aba
Joe Giampaoli <giampaoli.joe@gmail.com>
2020-11-18 17:32:51 -0800

    pdf generation function to each label button

--- pdfscript.js
+++ pdfscript.js
@@ -19,3 +19,73 @@ palletCustomerAddressStreet.innerText = obj[0].billStreet1;
 palletCustomerAddressCity.innerText = obj[0].billCity + ', ' + obj[0].billState + ', ' + obj[0].billZip;
 palletCustomerAddressPhone.innerText = obj[0].billPhoneNum;
 palletCustomerAddressPO.innerText = 'PO #: ' + obj[0].custPO;
+
+
+
+
+
+
+$('#download-packing-list-PDF').click(function () {
+
+    domtoimage.toPng(document.getElementById('packing-list'))
+    .then(function (blob) {
+
+        var pdf = new jsPDF('l', 'pt', [$('#packing-list').width(), $('#packing-list').height()]);
+        pdf.addImage(blob, 'PNG', 0, 0, $('#packing-list').width(), $('#packing-list').height());
+        pdf.save("test.pdf");
+        that.options.api.optionsChanged();
+    });    
+});
+
+$('#download-qa-PDF').click(function () {
+
+    domtoimage.toPng(document.getElementById('quality-assurance'))
+    .then(function (blob) {
+
+        var pdf = new jsPDF('l', 'pt', [$('#quality-assurance').width(), $('#quality-assurance').height()]);
+        pdf.addImage(blob, 'PNG', 0, 0, $('#quality-assurance').width(), $('#quality-assurance').height());
+        pdf.save("test.pdf");
+        that.options.api.optionsChanged();
+    });
+});
+
+$('#download-lc-PDF').click(function () {
+
+    domtoimage.toPng(document.getElementById('label-carton'))
+    .then(function (blob) {
+
+        var pdf = new jsPDF('l', 'pt', [$('#label-carton').width(), $('#label-carton').height()]);
+        pdf.addImage(blob, 'PNG', 0, 0, $('#label-carton').width(), $('#label-carton').height());
+        pdf.save("test.pdf");
+        that.options.api.optionsChanged();
+    });
+});
+
+$('#download-lr-PDF').click(function () {
+
+    domtoimage.toPng(document.getElementById('label-roll'))
+    .then(function (blob) {
+
+        var pdf = new jsPDF('l', 'pt', [$('#label-roll').width(), $('#label-roll').height()]);
+        pdf.addImage(blob, 'PNG', 0, 0, $('#label-roll').width(), $('#label-roll').height());
+        pdf.save("test.pdf");
+        that.options.api.optionsChanged();
+    });
+});
+
+$('#download-pallet-label-PDF').click(function () {
+
+    domtoimage.toPng(document.getElementById('pallet-label'))
+    .then(function (blob) {
+
+        var pdf = new jsPDF('l', 'pt', [$('#pallet-label').width(), $('#pallet-label').height()]);
+        pdf.addImage(blob, 'PNG', 0, 0, $('#pallet-label').width(), $('#pallet-label').height());
+        pdf.save("test.pdf");
+        that.options.api.optionsChanged();
+    });
+});
+
+
+
+
+
