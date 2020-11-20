a463dd73285b90e1d610e3b2d13d98ffa12170a0
Cory <coryfay97@gmail.com>
2020-11-19 16:40:31 -0800

    remove unneccessary comments,add relevant comments, clean up all code, remove excess files.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-    // START 'create labels for x amount of items'
-    // var numItemsInput = $('#numOfItems').val();
+    // ARRAY OF LABELS TO PULL FROM FOR INPUT CREATION
     var inputLabels = [
         'Tango Item #:',
         'Customer Item #',
@@ -22,7 +21,7 @@ $(document).ready(function () {
         'Quantity in Partial Box:',
         'Vendor/Supplier Name:'
     ];
-
+    // INPUT CREATION BASED ON VALUE OF NUMBER OF ITEMS
     $('.enterData').click(function () {
         event.preventDefault();
         var numItemsInput = $('#numOfItems').val();
@@ -31,92 +30,28 @@ $(document).ready(function () {
             p.text('Item ' + i);
             $('.items').append(p);
             var newDiv = $('<div>');
-            newDiv.attr('id', 'item'+i);
+            newDiv.attr('id', 'item' + i);
             $('.items').append(newDiv);
-            
+
             for (var k = 0; k < inputLabels.length; k++) {
-                //if (k === 0 ||k === 1 ||k === 2 ||k === 3 ||k === 4) {
-                    var div = $('<div>');
-                    div.addClass('input-field col s6 divCont' + (k + 1));
-                    $('#item'+i).append(div);
-                    var inp = $('<input>');
-                    inp.addClass('validate');
-                    inp.attr('type', 'text');
-                    inp.attr('id', 'inp' +(k+1));
-                    div.append(inp);
-                    var label = $('<label>');
-                    label.text(inputLabels[k]);
-                    label.attr('for', 'inp' + (k+1));
-                    div.append(label);
-                    //console.log(inputLabels.length);
-                //} //else if (k === 5){
-                //     var p2 = $('<p>');
-                //     p2.text(inputLabels[k]);
-                //     $('.emptyDiv').append(p2);
-                // }
-                
-                //else if (k === 2){
-                    // var p3 = $('<p>');
-                    // p3.text(inputLabels[k]);
-                    // $('.emptyDiv').append(p3);
-                    // var p4 = $('<p>');
-                    // p4.attr('id', 'p4');
-                    // var label4 = $('<label>');
-                    // //label4.attr('id', 'l4');
-                    // label4.attr('for', 'cutBand');
-                    // var inp4 = $('<input>');
-                    // inp4.attr('id', 'cutBand');
-                    // inp4.attr('name', 'cutOrTube');
-                    // inp4.attr('type', 'radio');
-                    // var span4 = $('<span>');
-                    // span4.text('Cut Bands');
-                    // inp4.html(span4);
-                    // label4.append(inp4);
-                    // p4.append(label4);
-                }
+                var div = $('<div>');
+                div.addClass('input-field col s6 divCont' + (k + 1));
+                $('#item' + i).append(div);
+                var inp = $('<input>');
+                inp.addClass('validate');
+                inp.attr('type', 'text');
+                inp.attr('id', 'inp' + (k + 1));
+                div.append(inp);
+                var label = $('<label>');
+                label.text(inputLabels[k]);
+                label.attr('for', 'inp' + (k + 1));
+                div.append(label);
             };
-        //};
+        };
     });
 
-    // PDF API 
-    $('#downloadPDF').click(function () {
-        domtoimage.toPng(document.getElementById('content2'))
-            .then(function (blob) {
-                var pdf = new jsPDF('l', 'pt', [$('#content2').width(), $('#content2').height()]);
-
-                pdf.addImage(blob, 'PNG', 0, 0, $('#content2').width(), $('#content2').height());
-                pdf.save("test.pdf");
-
-                that.options.api.optionsChanged();
-            });
-
-    });
-
-    // START 'if yes, displays hidden fields'
-    // BELOW --- Not Used In MVP
-    // $('#partialYes').on('click', function () {
-    //     event.preventDefault();
-    //     $('#hiddenPartialBox').removeClass('hide');
-    // });
-
-    // $('#partialNo').on('click', function () {
-    //     event.preventDefault();
-    //     $('#hiddenPartialBox').addClass('hide');
-    // });
-
-    // $('#yesPartialRoll').on('click', function () {
-    //     event.preventDefault();
-    //     $('#hiddenPartialRoll').removeClass('hide');
-    // });
-
-    // $('#noPartialRoll').on('click', function () {
-    //     event.preventDefault();
-    //     $('#hiddenPartialRoll').addClass('hide');
-    // });
-    // END 'if yes, displays hidden fields'
-
+    // CONVERT INPUTS TO JSON AND PUSH TO  var dataStorage/SAVE TO LOCAL STORAGE
     var dataStorage = [];
-    // convert input data to JSON 
     $('.submitBtn').click(function () {
         event.preventDefault();
         var companyData = {
@@ -166,16 +101,9 @@ $(document).ready(function () {
             totalWeight: $('#totalWeight').val(),
             tangoPackagingPONum: $('#tangoPackagingPONum').val(),
         };
-
-        //Trying to store old object data
-        // if (dataStorage !== null){
-        // var existing = localStorage.getItem('companyList');
-        // dataStorage.push(JSON.parse(existing));
-        // };
         dataStorage.push(companyData);
         localStorage.setItem('companyList', JSON.stringify(dataStorage));
         console.log(dataStorage);
-        // manipulate text on click
     });
 
 });
